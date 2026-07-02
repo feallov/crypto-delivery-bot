e5cfd75845876bf01a813653b031a8d5c1308c24
feallov <werr1on11@gmail.com>
2026-07-02 22:28:16 +0300

    Update strings.py

---
+++ strings.py
@@ -0,0 +1,14 @@
+texts = {
+    'en': {
+        'welcome': "<b>welcome to minimalist crypto tracker</b>",
+        'lang_set': "language set to english",
+        'price_up': "🟢",
+        'price_down': "🔴"
+    },
+    'ru': {
+        'welcome': "<b>добро пожаловать в минималистичный трекер</b>",
+        'lang_set': "язык изменен на русский",
+        'price_up': "🟢",
+        'price_down': "🔴"
+    }
+}
